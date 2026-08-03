d76cd2c248dd839b9c6db1631440d90c71e027fe
anna-minervae <abenbow2@illinois.edu>
2026-08-03 02:23:49 -0500

    Update script.js

--- CS 416/static/script.js
+++ CS 416/static/script.js
@@ -237,7 +237,7 @@ function Load_Bar_Chart(SubjectData) {
     var bar_svg = d3.select("#main_scene").append("div").append("svg");
     d3.select("svg").attr("width", width + 2 * margin).attr("height", height + 2 * margin).append("g").attr("transform", "translate(" + margin + "," + margin + ")");
 
-    var x = d3.scaleBand().domain(["-2", "-1.5", "-1", "-0.5", "0", "0.5", "1", "1.5", "2"]).range([0, width]);
+    var x = d3.scaleBand().domain(d => ["-2", "-1.5", "-1", "-0.5", "0", "0.5", "1", "1.5", "2"]).range([0, width]);
     bar_svg.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
 
     var y = d3.scaleLinear().domain([0, 1000]).range([0, height]);
@@ -251,7 +251,7 @@ function Load_Bar_Chart(SubjectData) {
     console.log(x(2));
 
 
-    bar_svg.append("text").attr("x", width).attr("y", height + 10).text("Difference in GPA Over 1 Semester").attr("text-anchor", "middle");
+    bar_svg.append("text").attr("x", width).attr("y", height + 25).text("Difference in GPA Over 1 Semester").attr("text-anchor", "start");
 
     d3.select("#main_scene").append("button").html(`Return to main scene`).attr("id", "return_button");
     document.getElementById("return_button").addEventListener("click", function() { Load_Scene(subjects[current_subject]);});
