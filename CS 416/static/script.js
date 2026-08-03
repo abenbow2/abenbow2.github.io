644f144f5adf7f56638ec5fa232dbdc9e9a78296
anna-minervae <abenbow2@illinois.edu>
2026-08-02 23:14:16 -0500

    Update script.js

--- CS 416/static/script.js
+++ CS 416/static/script.js
@@ -212,8 +212,9 @@ function Load_Bar_Chart(SubjectData) {
         }
     }
 
-    var bin_maker = d3.bin().domain([-2, 2]).thresholds(8);
+    var bin_maker = d3.bin().domain([-2, 2]).thresholds([-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2]);
     var bins_moreAI = bin_maker(bar_data_moreAI);
+    console.log(bins_moreAI);
 
     var bar_svg = d3.select("#main_scene").append("div").append("svg");
     d3.select("svg").attr("width", width + 2 * margin).attr("height", height + 2 * margin).append("g").attr("transform", "translate(" + margin + "," + margin + ")");
@@ -225,7 +226,7 @@ function Load_Bar_Chart(SubjectData) {
     bar_svg.append("g").attr("transform", "translate(" + margin + "," + margin + ")").call(d3.axisLeft(y));
 
     // MORE AI
-    bar_svg.selectAll("rect").data(bins_moreAI).enter().append("rect").attr("x", function(d, i) { return i; }).attr("y", function(d) { return d; }).attr("width", x.bandwidth()).attr("height", function(d) { return height - d; }).attr("fill", "#064A6F")
+    bar_svg.selectAll("rect").data(bins_moreAI).enter().append("rect").attr("x", function(d, i) { return x(d); }).attr("y", function(d, i) { return y(d); }).attr("width", x.bandwidth()).attr("height", function(d) { return height - d; }).attr("fill", "#064A6F")
     
 
     d3.select("#main_scene").append("button").html(`Return to main scene`).attr("id", "return_button");
