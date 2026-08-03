a215864cb0445f52a161d8da5468309dc6541165
anna-minervae <abenbow2@illinois.edu>
2026-08-02 21:47:06 -0500

    Update script.js

--- CS 416/static/script.js
+++ CS 416/static/script.js
@@ -107,8 +107,8 @@ function Load_Pie_Chart(SubjectData) {
     d3.select("#main_scene").html(null);
     d3.select("#main_scene").append("h2").html(`How Students in ${SubjectData.name} Use AI`);
     
-    const width = 500;
-    const height = 500;
+    const width = 600;
+    const height = 600;
     const radius = height / 2;
 
     // Copywriting, Summarizing, Debugging, Ideation, or Direct Answer
@@ -154,6 +154,7 @@ function Load_Pie_Chart(SubjectData) {
 
     var ai_usage_pie = d3.select("#main_scene").append("div").attr("id", "ai_usage_pie");
     var arc = d3.arc().innerRadius(0).outerRadius(radius);
+    var labelArc = d3.arc().outerRadius(radius - 40).innerRadius(radius - 40);
     var pie = d3.pie();
     var color_pie = ["#053A56", "#064A6F", "#0C84C6", "#39B2F3", "#90D4F9"];
 
@@ -161,10 +162,8 @@ function Load_Pie_Chart(SubjectData) {
     usage_pie_chart_svg.selectAll("path").data(pie(usage_data)).enter().append("path").attr("d", arc).attr("fill", function(d,i){return color_pie[i]});
 
     // labels based on https://d3-graph-gallery.com/graph/pie_annotation.html
-    usage_pie_chart_svg.selectAll("path").data(pie(usage_data)).enter().append('text').text(function(d, i){ return labels[i]; }).attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")";  })
-        .style("text-anchor", "middle")
-        .style("font-size", 18)
-        .style("color", "#ffffff");
+    //usage_pie_chart_svg.selectAll("path").data(pie(usage_data)).enter().append('text').text(function(d, i){ return labels[i]; }).attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")";  }).style("text-anchor", "middle").style("font-size", 18).style("color", "#ffffff");
+    usage_pie_chart_svg.append("text").attr("transform", function(d) { return "translate(" + labelArc.centroid(d) + ")"; }).text(function(d, i) { return labels[i];}).style("color", "#fff");
 
     d3.select("#ai_usage_pie").append("div").append("h4").attr("id", "pie_common").html(`Students in ${SubjectData.name} most commonly use AI for ${most_common.toLowerCase()}, followed by ${second_common.toLowerCase()}.`);
 
