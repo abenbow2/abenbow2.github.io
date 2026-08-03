dfd9ab345a4372e1eae3535ea00b84d2bb4125a2
anna-minervae <abenbow2@illinois.edu>
2026-08-03 00:04:26 -0500

    Update script.js

--- CS 416/static/script.js
+++ CS 416/static/script.js
@@ -237,16 +237,16 @@ function Load_Bar_Chart(SubjectData) {
     var bar_svg = d3.select("#main_scene").append("div").append("svg");
     d3.select("svg").attr("width", width + 2 * margin).attr("height", height + 2 * margin).append("g").attr("transform", "translate(" + margin + "," + margin + ")");
 
-    var x = d3.scaleBand().domain([-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2]).range([0, width]);
+    var x = d3.scaleBand().domain(["-2", "-1.5", "-1", "-0.5", "0", "0.5", "1", "1.5", "2"]).range([0, width]);
     bar_svg.append("g").attr("transform", "translate(0," + height + ")").call(d3.axisBottom(x));
 
     var y = d3.scaleLinear().domain([0, 1000]).range([0, height]);
     bar_svg.append("g").attr("transform", "translate(" + margin + "," + margin + ")").call(d3.axisLeft(y));
 
     // MORE AI
-    bar_svg.selectAll("rect").data(bar_data_moreAI).enter().append("rect").attr("x", function(d, i) { return x((i * 4) / 9 - 2); }).attr("y", function(d, i) { return y(d); }).attr("width", x.bandwidth()).attr("height", function(d) { return height - y(d); }).attr("fill", "#064A6F")
+    bar_svg.selectAll("rect").data(bar_data_moreAI).enter().append("rect").attr("x", function(d, i) { return x(d); }).attr("y", function(d, i) { return y(d); }).attr("width", x.bandwidth()).attr("height", function(d) { return height - y(d); }).attr("fill", "#064A6F")
     
-    bar_svg.append("text").attr("x", width).attr("y", height - 10).text("Difference in GPA Over 1 Semester");
+    bar_svg.append("text").attr("x", width).attr("y", height - 10).text("Difference in GPA Over 1 Semester").attr("text-anchor", "end");
 
     d3.select("#main_scene").append("button").html(`Return to main scene`).attr("id", "return_button");
     document.getElementById("return_button").addEventListener("click", function() { Load_Scene(subjects[current_subject]);});
